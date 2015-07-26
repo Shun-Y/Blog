e92270e8a1e565ce823179a28950c66276c32d52
shun yasumoto <vis.nt.rstd@gmail.com>
2015-07-26 16:44:11 +0900

    reset

--- deploy.sh
+++ deploy.sh
@@ -1,16 +1,17 @@
 ###Deploy to test environment
 
 #Circle CI Script for test
-docker-machine start test #activate test machine
-docker-machine eval $(docker-machine env test) #point at test machine
-docker-compose -f test.yml pull web
-docker-compose -f test.yml up --no-deps -d
+#docker-machine start test #activate test machine
+#eval $(docker-machine env test) #point at test machine
+#docker-compose -f test.yml pull web
+#docker-compose -f test.yml up --no-deps -d
 #Check or return test result.
 #If test result is True, deproy to production(staging)
 
 docker-machine start production
-docker-machine eval $(docker-machine env production) #point at test machine
-docker-compose -f production.yml kill web
+eval $(docker-machine env production) #point at test machine
+# docker-compose -f production.yml kill web
+docker-compose -f production.yml stop web
 docker-compose -f production.yml rm -f -v web
 docker-compose -f production.yml pull web
-docker-compose -f production.yml up --no-deps -d
+docker-compose -f production.yml up -d --no-deps web
--- edit.sh
+++ edit.sh
@@ -43,14 +43,15 @@ git checkout master
 git pull # To make sure master is up-to-date.
 git pull origin $task # To merge the central repository’s copy of<TASK>
 #check whether bug exists or not.
-while (confirm "tell me when pull request finished \n"); do
+while (confirm "tell me you checked whether there exists bug\n"); do
     echo "finished?\n"
 done
 git push origin master # To push back to origin master
 
 #push to docker repository
-docker-compose run web stack clean && docker-compose run web stack build && docker-compose run web stack install
-sudo docker-compohse build --no-cache web
-docker commit $(docker-compose ps -q web) blog_web:latest
+docker-compose run --rm --no-deps web stack clean && docker-compose run --rm --no-deps web stack build && docker-compose run --rm --no-deps web stack install
+sudo docker-compose build --no-cache web
 docker tag -f blog_web:latest shuny/blog_web:latest
+docker images
+docker login
 docker push shuny/blog_web:latest
